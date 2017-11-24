fce0fad6e64c1bf06a04e7dc7f90eeb3cf0afe0a
Angel Beltran <beltranbeaverdam@gmail.com>
2017-11-23 16:49:33 -0800

    added game of life

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-//import './App.css';
 import Asteroids from 'asteroids'
 import Tetris from 'angels-tetris'
+import GameOfLife from 'angels-game-of-life'
 
 
 function SidebarTab ({ onClick, onMouseOver, onMouseLeave, children }) {
@@ -87,9 +87,9 @@ class Sidebar extends Component {
     }
 
     const tabContent = [
+      'Game of Life',
       'Asteroids',
       'Tetris',
-      'Tab 3',
     ]
 
     return (
@@ -123,11 +123,170 @@ class Sidebar extends Component {
 }
 
 
+class GameOfLifeWrapper extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      paused: false,
+      protected: false,
+      savedGrid: null,
+      rows: 20,
+      columns: 20,
+    }
+  }
+
+  pause = () => {
+    this.setState({ paused: !this.state.paused })
+  }
+
+  protect = () => {
+    this.setState({ protected: !this.state.protected })
+  }
+
+  saveGrid = (grid) => {
+    this.setState({ savedGrid: grid })
+  }
+
+  updateRows = (n) => {
+    if (n < 0) {
+      this.setState({ rows: 0 })
+    } else {
+      this.setState({ rows: n })
+    }
+  }
+
+  updateColumns = (n) => {
+    if (n < 0) {
+      this.setState({ columns: 0 })
+    } else {
+      this.setState({ columns: n })
+    }
+  }
+
+  getClearPromise = () => {
+    let _resolve
+
+    const createClearPromise = () => {
+      return new Promise((resolve) => {
+        _resolve = resolve
+        if (this.state.clear !== clear) {
+          this.setState({ clear })
+        }
+      })
+    }
+
+    function clear () {
+      const resolve = _resolve
+      resolve(createClearPromise)
+    }
+
+    return createClearPromise()
+  }
+
+  getResetPromise = () => {
+    let _resolve
+
+    const createResetPromise = () => {
+      return new Promise((resolve) => {
+        _resolve = resolve
+        if (this.state.reset !== reset) {
+          this.setState({ reset })
+        }
+      })
+    }
+
+    function reset () {
+      const resolve = _resolve
+      resolve(createResetPromise)
+    }
+
+    return createResetPromise()
+  }
+
+  render() {
+    return (
+      <div style={{
+        width: '100%',
+        height: '80%',
+        //position: 'absolute',
+        //top: '10%',
+        //left: '10%',
+        //backgroundColor: '#cceedd',
+      }}>
+        <GameOfLife
+          width="100%"
+          height="100%"
+          rows={this.state.rows}
+          columns={this.state.columns}
+          startingGrid={this.state.savedGrid}
+          paused={this.state.paused}
+          protected={this.state.protected}
+          saveGrid={this.saveGrid}
+          clear={this.getClearPromise}
+          reset={this.getResetPromise}
+        />
+
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+          <div>
+            <button onClick={this.pause}>
+              {this.state.paused ? 'unpause' : 'pause'}
+            </button>
+            <button onClick={this.protect}>
+              {this.state.protected ? 'unprotect' : 'protect'}
+            </button>
+            <button onClick={this.state.clear}>clear</button>
+            <button onClick={this.state.reset}>reset</button>
+          </div>
+
+          <div>
+            rows: <input
+              type="number"
+              value={this.state.rows}
+              onChange={(e) => this.updateRows(e.target.value)}
+            />
+            <button
+              onClick={() => this.updateRows(this.state.rows - 1)}
+            >
+              -
+            </button>
+            <button
+              onClick={() => this.updateRows(this.state.rows + 1)}
+            >
+              +
+            </button>
+          </div>
+
+          <div>
+            columns: <input
+              type="number"
+              value={this.state.columns}
+              onChange={(e) => this.updateColumns(e.target.value)}
+            />
+            <button
+              onClick={() => this.updateColumns(this.state.columns - 1)}
+            >
+              -
+            </button>
+            <button
+              onClick={() => this.updateColumns(this.state.columns + 1)}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
 function Body ({ page }) {
   let content = ''
   if (page === 0) {
-    content = <Asteroids />
+    content = <GameOfLifeWrapper />
   } else if (page === 1) {
+    content = <Asteroids />
+  } else if (page === 2) {
     content = <Tetris />
   }
 
@@ -144,7 +303,8 @@ class App extends Component {
     super(props)
 
     this.state = {
-      page: -1,            // which page is being shown (-1 <=> no pages selected); n-1 <=> nth page selected)
+      //page: -1,            // which page is being shown (-1 <=> no pages selected); n-1 <=> nth page selected)
+      page: 0,            // which page is being shown (-1 <=> no pages selected); n-1 <=> nth page selected)
     }
   }
 
@@ -171,10 +331,10 @@ class App extends Component {
     return (
       <div style={appStyle}>
         <div className="row no-gutters" style={{ height: '100%' }}>
-          <div className="col-1 col-2-xs" style={sidebarWrapperStyle}>
+          <div className="col-2 col-2-sm" style={sidebarWrapperStyle}>
             <Sidebar onTabMouseClick={this.onTabMouseClick}/>
           </div>
-          <div className="col-11 col-10-xs" style={bodyWrapperStyle}>
+          <div className="col-10 col-10-sm" style={bodyWrapperStyle}>
             <Body page={this.state.page}/>
           </div>
         </div>
