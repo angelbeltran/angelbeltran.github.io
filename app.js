5edb21a5f90f8d9cf7cb886a5e7d66ff018cae7a
Angel Beltran <beltranbeaverdam@gmail.com>
2018-06-20 03:11:26 -0700

    added recognized routes and redirection on unrecognized routes to app.js

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
-app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
-app.use(express.static('build'));
+// logging: standard Apache combined log output
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
+
+// all routes served from save static html
+const staticMiddleware = express.static('build');
+['/', '/home', '/about', '/contact', '/fun-stuff'].forEach(function (route) {
+  app.use(route, staticMiddleware);
+});
+
+// unrecognizes routes get redirected to the top route
+app.get('*', function (req, res) {
+  res.redirect('/');
+});
 
 // Start the server
 const PORT = process.env.PORT || 8080;
